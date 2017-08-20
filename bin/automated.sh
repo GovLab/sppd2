ea56ec863aba4267a0b0453fb2f7493215c2ee74
Jim Wrubel <jwrubel@gmail.com>
2017-08-20 14:58:36 -0400

    lets try a totally different baseline for automation script

--- bin/automated.sh
+++ bin/automated.sh
@@ -1,24 +1,25 @@
-# Copy static site
-CWD=`pwd`
-
-# Clone Pages repository
-cd /tmp
-git clone git@github.com:jwrubel/ga-contentful.git build
-# cd build && git checkout -b YOUR_BRANCH origin/YOUR_BRANCH # If not using master
-
-# Trigger Jekyll rebuild
-cd $CWD
+echo '******************************************'
+echo '**** Updating content from Contentful ****'
+echo '******************************************'
 CONTENTFUL_ACCESS_TOKEN=${ENV_CONTENTFUL_ACCESS_TOKEN} CONTENTFUL_SPACE_ID=${ENV_CONTENTFUL_SPACE_ID} bundle exec jekyll contentful
 bundle exec jekyll build
 
-# Push newly built repository
-cp -r $CWD/_site/* /tmp/build
+git --no-pager diff
 
-cd /tmp/build
+echo '******************************************'
+
+echo '*************************************************'
+echo '**** Publish updated content to Github Pages ****'
+echo '*************************************************'
 
 git config --global user.email "jwrubel@gmail.com"
 git config --global user.name "James Wrubel"
 
+echo '**** Committing updated content ****'
 git add .
-git commit -m "Automated Rebuild"
+git commit -m "Automated Publish"
+
+echo '**** Pushing updated content to Github ****'
 git push -f origin master
+
+echo '*************************************************'
