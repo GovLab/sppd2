1ba0c5ca266b425334e636412a27bee26b90e463
Jim Wrubel <jwrubel@gmail.com>
2017-08-21 15:31:54 -0400

    add more commenting around the automated build step

--- bin/automated.sh
+++ bin/automated.sh
@@ -2,15 +2,22 @@ echo '******************************************'
 echo '**** Updating content from Contentful ****'
 echo '******************************************'
 CONTENTFUL_ACCESS_TOKEN=${ENV_CONTENTFUL_ACCESS_TOKEN} CONTENTFUL_SPACE_ID=${ENV_CONTENTFUL_SPACE_ID} bundle exec jekyll contentful
+echo '******************************************'
+echo '****         rebuilding site          ****'
+echo '******************************************'
 bundle exec jekyll build
+
+echo '******************************************'
+echo '****         diffing content          ****'
+echo '******************************************'
 
 git --no-pager diff
 
 echo '******************************************'
 
-echo '*************************************************'
-echo '**** Publish updated content to Github Pages ****'
-echo '*************************************************'
+echo '******************************************'
+echo '**** Publish updates to Github Pages  ****'
+echo '******************************************'
 
 git config --global user.email "jwrubel@gmail.com"
 git config --global user.name "James Wrubel"
