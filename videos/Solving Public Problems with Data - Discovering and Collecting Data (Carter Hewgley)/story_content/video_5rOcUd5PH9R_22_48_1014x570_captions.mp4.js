f54d47c82aa5c2e895e0b9feb002951c446ea642
mocxd <mark.adkins.hastings@gmail.com>
2017-11-14 11:09:09 -0800

    update videos

---
+++ videos/Solving Public Problems with Data - Discovering and Collecting Data (Carter Hewgley)/story_content/video_5rOcUd5PH9R_22_48_1014x570_captions.mp4.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"1%0A00:00:00,77%20--%3E%2000:00:04,78%0ALet%E2%80%99s%20start%20with%20seven%20practical%20tips%20for%20discovering%20and%20collecting%20data.%0A%0A2%0A00:00:04,78%20--%3E%2000:00:09,95%0AI%E2%80%99ll%20drill%20into%20greater%20detail%20on%20the%20seventh%20tip%20with%20a%20quick%20primer%20on%20conducting%20surveys.%0A%0A"}')
